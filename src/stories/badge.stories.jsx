bd7b2560cb2e7adecc4584479b9833d32033eff8
Sivadass N <nsivadass@gmail.com>
2023-11-13 17:37:49 +0530

    feat(storybook): Added badge story

---
+++ src/stories/badge.stories.jsx
@@ -0,0 +1,12 @@
+import Badge from "../components/button/Badge";
+
+const meta = {
+  component: Badge,
+};
+
+export const Default = {
+  name: "Default",
+  render: () => <Badge label="new" />,
+};
+
+export default meta;
