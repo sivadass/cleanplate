4334bfe65b9c52d970254d53e7c4f83ca7691e21
Sivadass N <nsivadass@gmail.com>
2024-03-10 14:02:51 +0530

    fix(avatar): Fixed babel error

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import Icon from "../icon";
 import styles from "./Avatar.module.css";
-import { getInitials } from "../../utils/common";
+
+const getInitials = (fullName = "") => {
+  const initial = fullName?.[0] || "";
+  return initial.toUpperCase();
+};
 
 const Avatar = ({
   size = "medium",
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,12 +1,4 @@
 export const getInitials = (fullName = "") => {
-  const [firstName, secondName] = fullName.split(" ");
-  let initials = "";
-  if (!secondName) {
-    initials = firstName?.[0] || "";
-  } else if (!firstName && !secondName) {
-    initials = "";
-  } else {
-    initials = `${firstName?.[0] || ""}${secondName?.[0] || ""}`;
-  }
-  return initials.toUpperCase();
+  const initial = fullName?.[0] || "";
+  return initial.toUpperCase();
 };
